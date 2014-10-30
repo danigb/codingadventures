ec5e570b82a9c3b5599b8e29a8c05272fdae3fbc
danigb <danigb@gmail.com>
2014-10-30 23:27:35 +0100

    Assets directory

--- config.rb
+++ config.rb
@@ -38,58 +38,11 @@ end
 
 page "/feed.xml", layout: false
 
-###
-# Compass
-###
+set :css_dir, 'assets/stylesheets'
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+set :js_dir, 'assets/javascripts'
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :images_dir, 'assets/images'
 
 # Build-specific configuration
 configure :build do
