10a566c422bc97353eb7f0a36d045d5365eb7c7a
danigb <danigb@gmail.com>
2015-01-25 23:48:09 +0100

    Can't get requestAnimationFrame working with reactiveX, argggg

--- source/assets/javascripts/experiments/rxjs-dom-requestanimationframe.js
+++ source/assets/javascripts/experiments/rxjs-dom-requestanimationframe.js
@@ -1,5 +1,41 @@
 (function() {
   'use strict';
 
-  alert('ea');
+  var canvas, context, subscription;
+
+  function init() {
+    canvas = document.createElement('canvas');
+    canvas.width = 600;
+    canvas.height = 400;
+    context = canvas.getContext('2d');
+    document.body.appendChild(canvas);
+  }
+
+  function animate() {
+    subscription = Rx.Observable.generate(
+      0,
+      function(x) { return true; },
+      function(x) { return x + 1; },
+      function(x) { return x; }
+    )
+    .timestamp()
+    .throttle(500)
+    .subscribe(draw);
+  }
+
+  function draw(ts) {
+    var time = ts.timestamp * 0.002;
+    var x = Math.sin( time ) * 192 + 256;
+    var y = Math.cos( time * 0.9 ) * 192 + 256;
+    context.fillStyle = ts.value % 2 === 0 ? 'rgb(200,200,20)' : 'rgb(20,20,200)';
+    context.beginPath();
+    context.arc( x, y, 10, 0, Math.PI * 2, true );
+    context.closePath();
+    context.fill();
+  }
+
+  window.onload = function() {
+    init();
+    animate();
+  }
 })();
--- source/experiments/index.html.md
+++ source/experiments/index.html.md
@@ -26,5 +26,5 @@ layout: base
 http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
 http://krazydad.com/tutorials/makecolors.php
 
-### Generatos
+### Generators
 http://stackoverflow.com/questions/23096064/how-can-i-feature-detect-es6-generators
